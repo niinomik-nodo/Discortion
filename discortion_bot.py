49aff08076c825939cbb833776ddb02eead08372
niinomik-nodo <k.niinomi.nodo@gmail.com>
2023-06-21 18:30:47 +0000

    fixed error response message content

--- discortion_bot.py
+++ discortion_bot.py
@@ -10,8 +10,10 @@ from notion_client import Client
 
 import sys
 import logging
+
 logger = logging.getLogger()
 
+notion = None
 
 error_dict = {
     "annaounce": {
@@ -27,28 +29,40 @@ error_dict = {
                      "・3桁0埋めの数字で入力してください。\n\n"\
                      "(例) /curr 001",
 
+            "excess": "想定されていないフォーマットのコマンドを検知しました。\n"\
+                      "下記をご確認の上、再度コマンドを入力してください。\n\n"\
+                      "・コマンドに設定する数字は「327」以下にしてください。\n\n"\
+                      "・カリキュラム番号がわからない場合、Wikiの「カリキュラム対応表」をご確認ください。\n\n"\
+                      "(例) /curr 327",
+
         },
         "user": {
             "digit": "想定されていないフォーマットの名前を検知しました。\n"\
                      "下記をご確認の上、再度コマンドを入力してください。\n\n"\
                      "・5桁0埋めの数字で設定してください。\n\n"\
                      "(例) テスト太郎_00001\n\n"\
-                     "ユーザ名の設定方法は下記URLからご確認ください。\n"\
+                     "下記リンクを参考に「サーバープロフィール」タブからサーバーニックネームを変更してください。\n"\
                      "https://setup-lab.net/discord-nickname-change/",
 
             "_": "想定されていないフォーマットの名前を検知しました。\n"\
                  "下記をご確認の上、再度コマンドを入力してください。\n\n"\
                  "・氏名と数字の間は「_」で設定してください。\n\n"\
                  "(例) テスト太郎_00001\n\n"\
-                 "ユーザ名の設定方法は下記URLからご確認ください。\n"\
+                 "下記リンクを参考に「サーバープロフィール」タブからサーバーニックネームを変更してください。\n"\
                  "https://setup-lab.net/discord-nickname-change/",
 
             "nodigit": "想定されていないフォーマットの名前を検知しました。\n"\
                        "下記をご確認の上、再度コマンドを入力してください。\n\n"\
                        "・氏名の後にご自身のDBに対応する5桁0埋めの数値を設定してください。\n\n"\
                        "(例) テスト太郎_00001\n\n"\
-                       "ユーザ名の設定方法は下記URLからご確認ください。\n"\
+                       "下記リンクを参考に「サーバープロフィール」タブからサーバーニックネームを変更してください。\n"\
                        "https://setup-lab.net/discord-nickname-change/",
+
+            "dbidkey": "想定されていないフォーマットの名前を検知しました。\n"\
+                       "下記をご確認の上、再度コマンドを入力してください。\n\n"\
+                       "・氏名の後にご自身のDBに対応する5桁0埋めの数値を設定してください。\n\n"\
+                       "(例) テスト太郎_00001\n\n"\
+                       "番号がご不明の番号の場合、担当のエージェントにご確認ください。",
 
             "nickname": "discord上に表示させる名前を「サーバープロフィール」に設定してください。\n\n"\
                         "ユーザ名の設定方法は下記URLからご確認ください。\n"\
@@ -60,7 +74,7 @@ error_dict = {
                 "(例)\n"\
                 "・コマンド: /curr 001\n"\
                 "・名前: テスト太郎_00001\n\n"\
-                "ユーザ名の設定方法は下記URLからご確認ください。\n"\
+                "下記リンクを参考に「サーバープロフィール」タブからサーバーニックネームを変更してください。\n"\
                 "https://setup-lab.net/discord-nickname-change/",
 
         "unknown": "不明なエラーを検知しました。\n"\
@@ -68,24 +82,27 @@ error_dict = {
                    "(例)\n"\
                    "・コマンド: /curr 001\n"\
                    "・名前: テスト太郎_00001\n\n"\
-                   "エラーが解消されない場合、運営にお問い合わせください。",
+                   "エラーが解消されない場合、「問い合わせ」チャットにてご連絡ください。",
 
     },
     "message": {
         "command": {
             "/curr": "[/curr ] could not found.",
             "digit": "Integer Missing.",
+            "excess": "Too Bigger index has detected.",
         },
         "user": {
             "digit": "Integer Missing.",
             "_": "Underscore has not detected.",
             "nodigit": "Name has no digit.",
             "nickname": "Ncikname and Displayname did not match.",
+            "dbidkey": "Non-existent db key has detected.",
         },
         "both": "command and name detect error.",
         "unknown": "Unknown error has detected.",
     }
 }
+
 
 # set error handler
 # get save dir
@@ -96,16 +113,17 @@ def set_handler():
     save_file_name = str(now)[:10].replace('-', '') + '.log'  # YYYYMMDD.log
 
     # get save dir
-    save_dir = '/var/log/notion_api'
+    save_dir = 'notion_api_logs'
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
-    save_dir_name =  os.path.join(save_dir, save_file_name)
+    save_dir_name = os.path.join(save_dir, save_file_name)
 
     # set logger level
     logger.setLevel(logging.ERROR)
 
     # set error handler
-    formatter = logging.Formatter('%(asctime)s %(lineno)s %(message)s', '%Y/%m/%d %H:%M:%S')
+    formatter = logging.Formatter('%(asctime)s %(lineno)s %(message)s',
+                                  '%Y/%m/%d %H:%M:%S')
     fh = logging.FileHandler(filename=save_dir_name, encoding='utf-8')
     fh.setLevel(logging.ERROR)
     fh.setFormatter(formatter)
@@ -128,14 +146,18 @@ def get_message(message):
 
         # check error
         err_message_command, err_announce_command = check_command(command)
-        err_message_username, err_annaounce_username = check_user(get_messemger_name)
+        err_message_username, err_annaounce_username = check_user(
+            get_messemger_name)
 
         # no error
-        if not ( bool(err_message_command) | bool(err_message_username)):
+        if not (bool(err_message_command) | bool(err_message_username)):
 
-            user_name = get_messemger_name.split("_")[0]  # explicit full name from displaied user name
-            database_id = database_id_list[get_messemger_name.split("_")[1]]  # explicit database id from displaied user name
-            curr_number = command[6:].zfill(3)  # explicit curriculum number from command statement
+            user_name = get_messemger_name.split("_")[
+                0]  # explicit full name from displaied user name
+            database_id = database_id_list[get_messemger_name.split(
+                "_")[1]]  # explicit database id from displaied user name
+            curr_number = command[6:].zfill(
+                3)  # explicit curriculum number from command statement
 
             return user_name, database_id, curr_number, error_annaounce
 
@@ -162,8 +184,9 @@ def get_message(message):
     except Exception as e:
 
         # get error log
-        print("exception e(___DEBGU___): ", e)
-        err_line = 'command: {}, error message: {}, username: {}'.format(command, e, get_messemger_name)
+        print("exception e(___DEBUG___): ", e)
+        err_line = 'command: {}, error message: {}, username: {}'.format(
+            command, e, get_messemger_name)
         logger.error(err_line)
 
         # unknown error
@@ -183,6 +206,9 @@ def check_command(command):
     elif not command[6:].isdigit():
         err_message = error_dict["message"]["command"]["digit"]
         err_annaounce = error_dict["annaounce"]["command"]["digit"]
+    elif int(command[6:]) > 327:
+        err_message = error_dict["message"]["command"]["excess"]
+        err_annaounce = error_dict["annaounce"]["command"]["excess"]
     return err_message, err_annaounce
 
 
@@ -199,26 +225,23 @@ def check_user(messemger_name):
     elif messemger_name[-6] != '_':
         err_message = error_dict["message"]["user"]["_"]
         err_annaounce = error_dict["annaounce"]["user"]["_"]
+    elif not messemger_name[-5:] in database_id_list.keys():
+        err_message = error_dict["message"]["user"]["dbidkey"]
+        err_annaounce = error_dict["annaounce"]["user"]["dbidkey"]
     return err_message, err_annaounce
 
 
 def main():
-    # Discord_Token
-    # TOKEN = os.environ['DISCORD_KEY']
-    # TOKEN = 'MTA5ODEwNDI1OTkyNjU3MzA3Ng.G7pCDo.e16s9QEh33C6ELIvdE4JGBRtbTrv1zpZNze7MM'
-    TOKEN = "MTExMzA5NjUwMDA4NDQ5MDMyMQ.GT9WTH.TcHOQQ9sRQkgNE1lBTNe0sCpyd9P9Q2m_CVSkg"
-
-    # Notion_token
-    # NOTION_TOKEN = os.environ['NOTION_KEY']
+    # Define Notion_api
     NOTION_TOKEN = 'secret_013tQKTmEmdOOWcBgcPBTKNN3wAOzgQhTSkN2t63M2U'
 
-    # Initializing Notion Client
-    notion = Client(auth=NOTION_TOKEN)
+    # Discord_Token
+    # TOKEN = os.environ['DISCORD_KEY']
+    TOKEN = "MTA5ODEwNDI1OTkyNjU3MzA3Ng.Gc02V0.f9p0lk0nVBAX9rfJvi-Ad56feQ3Rvt4yXxd-Z8"
 
-    # Discord Channel ID 
-    # CHANNEL_ID = "1098841750077968435"  # daily report
-    # CHANNEL_ID = "1098157558885273621"
-    CHANNEL_ID = "1119239857751986227"  # bot_bot
+    # Discord Channel ID
+    CHANNEL_ID = "1098841750077968435"  # daily report
+    # CHANNEL_ID = "1119239857751986227"  # bot_bot
 
     # 接続に必要なオブジェクトを生成
     client = discord.Client(intents=discord.Intents.all())
@@ -229,106 +252,120 @@ def main():
     # 起動時に動作する処理
     @client.event
     async def on_ready():
+        global notion
         # 起動したらターミナルにログイン通知が表示される
         print('ログインしました')
+        # Initializing Notion Client
+        # Notion_token
+        # NOTION_TOKEN = os.environ['NOTION_KEY']
+        notion = Client(auth=NOTION_TOKEN)
 
     # 発言時に実行されるイベントハンドラを定義
     @client.event
     async def on_message(message):
 
-        # check commanded user
-        # (ignore process for bot and Administrator)
-        if (message.author != client.user) & (message.author.display_name[-3:] != "@運営"):
+        # Check if the message is from the desired channel
+        if message.channel.id == int(CHANNEL_ID):
 
-            # コマンドに対応するデータを取得して表示
-            username, database_id, curr_number, err_announce = get_message(message)
+            # check commanded user
+            # (ignore process for bot and Administrator)
+            if (message.author !=
+                    client.user) & (message.author.display_name[-3:] != "@運営"):
 
-            # send error message to user in chat
-            if bool(err_announce):
-                await message.channel.send(err_announce)
-                return
+                # コマンドに対応するデータを取得して表示
+                username, database_id, curr_number, err_announce = get_message(
+                    message)
 
-            try:
-                # Check if the message is from the desired channel
-                if message.channel.id == int(CHANNEL_ID):
+                # send error message to user in chat
+                if bool(err_announce):
+                    await message.channel.send(err_announce)
+                    return
+
+                try:
+                    # Check if the message is from the desired channel
+                    if message.channel.id == int(CHANNEL_ID):
+
+                        # Check if the username retrieved from Discord exists in Notion's database.
+                        results = notion.databases.query(
+                            **{
+                                "database_id": database_id,
+                                "filter": {
+                                    "property": "名前",
+                                    "title": {
+                                        "contains": username,
+                                    },
+                                },
+                            }).get("results")
+
+                    if not results:
+                        # Notion Page Properties
+                        create_page_properties = {
+                            '名前': {
+                                'title': [{
+                                    'text': {
+                                        'content': username
+                                    }
+                                }]
+                            },
+                            'カリキュラムNo': {
+                                'rich_text': [{
+                                    'text': {
+                                        # 'content': trans_curr_no
+                                        'content': curr_number
+                                    }
+                                }]
+                            },
+                            '更新日': {
+                                'date': {
+                                    'start':
+                                    datetime.now().strftime('%Y-%m-%d')
+                                }
+                            }
+                        }
+
+                        # Adding the page to Notion Database
+                        notion.pages.create(
+                            parent={'database_id': database_id},
+                            properties=create_page_properties)
+                        print("クリエイト成功")
 
                     # Check if the username retrieved from Discord exists in Notion's database.
-                    results = notion.databases.query(
-                        **{
-                            "database_id": database_id,
-                            "filter": {
-                                "property": "名前",
-                                "title": {
-                                    "contains": username,
-                                },
+                    else:
+                        page_id = results[0]["id"]
+
+                        update_page_properties = {
+                            'カリキュラムNo': {
+                                'rich_text': [{
+                                    'text': {
+                                        # 'content': trans_curr_no
+                                        'content': curr_number
+                                    }
+                                }]
                             },
-                        }).get("results")
-
-                if not results:
-                    # Notion Page Properties
-                    create_page_properties = {
-                        '名前': {
-                            'title': [{
-                                'text': {
-                                    'content': username
+                            '更新日': {
+                                'date': {
+                                    'start':
+                                    datetime.now().strftime('%Y-%m-%d')
                                 }
-                            }]
-                        },
-                        'カリキュラムNo': {
-                            'rich_text': [{
-                                'text': {
-                                    # 'content': trans_curr_no
-                                    'content': curr_number
-                                }
-                            }]
-                        },
-                        '更新日': {
-                            'date': {
-                                'start': datetime.now().strftime('%Y-%m-%d')
                             }
                         }
-                    }
 
-                    # Adding the page to Notion Database
-                    notion.pages.create(parent={'database_id': database_id},
-                                        properties=create_page_properties)
-                    print("クリエイト成功")
+                        # Update to the property to Notion Database
+                        notion.pages.update(page_id=page_id,
+                                            properties=update_page_properties)
 
-                # Check if the username retrieved from Discord exists in Notion's database.
-                else:
-                    page_id = results[0]["id"]
+                        # notion.pages.create(parent={'database_id': DATABASE_ID}, properties=create_page_properties)
+                        print("アップデート成功")
 
-                    update_page_properties = {
-                        'カリキュラムNo': {
-                            'rich_text': [{
-                                'text': {
-                                    # 'content': trans_curr_no
-                                    'content': curr_number
-                                }
-                            }]
-                        },
-                        '更新日': {
-                            'date': {
-                                'start': datetime.now().strftime('%Y-%m-%d')
-                            }
-                        }
-                    }
-
-                    # Update to the property to Notion Database
-                    notion.pages.update(page_id=page_id,
-                                        properties=update_page_properties)
-
-                    # notion.pages.create(parent={'database_id': DATABASE_ID}, properties=create_page_properties)
-                    print("アップデート成功")
-
-            except Exception as e:
-                print("exception e: ", e)
-                command = message.content
-                get_messemger_name = message.author.display_name
-                err_line = 'command: {}, error message: {}, username: {}'.format(command, e, get_messemger_name)
-                logger.error(err_line)
-        else:
-            return
+                except Exception as e:
+                    print("exception e: ", e)
+                    command = message.content
+                    get_messemger_name = message.author.display_name
+                    err_line = 'command: {}, error message: {}, username: {}'.format(
+                        command, e, get_messemger_name)
+                    logger.error(err_line)
+            else:
+                return
 
     keep_alive()
     # connect to Discord server
